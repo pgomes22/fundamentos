c2082b192c929b912f2c9bfa47df2c76aa6ef413
Pedro Gomes <pedrogac@hotmail.com>
2023-03-23 23:20:49 -0300

    add array examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let notas: [f32; 4] = [6.5; 4];
+    let inteiro: usize = 0;
+
+    println!("{}", notas[inteiro]);
+
+    for indice in 0..notas.len() {
+        println!("A nota {} é = {}", indice + 1, notas[indice]);
+    }
+
+    matriz();
+}
+
+fn matriz() {
+    let matriz: [[f32; 3]; 2] = [
+        [0.0, 1.2, 0.1],
+        [1.3, 0.3, 1.4]
+    ];
+
+    for linha in matriz {
+        for item in linha {
+            println!("Item = {}", item);
+        }
+    }
 }
